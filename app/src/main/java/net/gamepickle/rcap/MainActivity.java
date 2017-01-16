8ca7f73a2127ddd7e635a531a220128b66b1839e
eliott07 <eliott07@gmail.com>
2017-01-16 19:08:29 +0100

    added new activity matchsimulation

--- app/src/main/java/net/gamepickle/rcap/MainActivity.java
+++ app/src/main/java/net/gamepickle/rcap/MainActivity.java
@@ -1,10 +1,13 @@
 package net.gamepickle.rcap;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.support.v4.util.Pair;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
     GameVariables gameVariables;
@@ -17,8 +20,15 @@ public class MainActivity extends AppCompatActivity {
         gameVariables.setPlayer_stats(50);
         gameVariables.setPlayer_happiness(50);
 
-        Button changeScreen;
-        //changeScreen = new Button().findViewById(R.id.Changing_Room);
+        ImageButton changeScreen = (ImageButton) findViewById(R.id.Changing_Room);
+
+        changeScreen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent match=new Intent(getApplicationContext(), MatchSimulation.class);
+                startActivity(match);
+            }
+        });
     }
 
         public Pair<Integer, Integer> match(){
---
+++ app/src/main/java/net/gamepickle/rcap/MatchSimulation.java
@@ -0,0 +1,13 @@
+package net.gamepickle.rcap;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MatchSimulation extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_match_simulation);
+    }
+}
