5266817054b365e845c04d02cd0063727af6269d
Joseph Chotard <joseph@gamepickle.net>
2017-01-27 19:04:11 +0100

    New Main
    Upgraded the main class to support the new global Variables class

--- app/src/main/java/net/gamepickle/rcapmanager/MainActivity.java
+++ app/src/main/java/net/gamepickle/rcapmanager/MainActivity.java
@@ -1,15 +1,27 @@
-package net.gamepickle.rcapmanager;
+package net.gamepickle.rcap;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
-import net.gamepickle.rcap.R;
+import static net.gamepickle.rcap.R.id.Name;
+import static net.gamepickle.rcap.R.id.cash_text;
+import static net.gamepickle.rcap.R.id.players_text;
+import static net.gamepickle.rcap.R.id.popularity_text;
 
 public class MainActivity extends AppCompatActivity {
-
+    GameVariables gameVars;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initialiseHeader();
+    }
+
+    private void initialiseHeader() {
+        gameVars = new GameVariables(this);
+        gameVars.setInt("CASH", 2, cash_text);
+        gameVars.setInt("POPULARITY", 42, popularity_text);
+        gameVars.setInt("PLAYER_STATS", 3, players_text);
+        gameVars.setString("NAME", gameVars.getString("NAME"), Name);
     }
 }
